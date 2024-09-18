e79c7dd5cbba573238f9346d02317d6350c54b63
Anonymous <anonymous@example.com>
2024-09-18 15:20:04 +0800

    Simplify scripts

--- CConvBig_verify.py
+++ CConvBig_verify.py
@@ -2,7 +2,6 @@ import os
 import re
 import subprocess
 from typing import List, Literal
-from typing_extensions import TypeAlias
 
 VIRTUAL_ENV_PATH = "/home/yuyi/loris5/PRIMA/venv_3dot8/bin/activate"
 """Path to activate Python virtual env."""
@@ -17,43 +16,35 @@ IMG_IDS: List[int] = [1,13,14,18,21,33,34,46,48,50,56,66,72,81,89,92,98,102,103,
 SPARSE_N: int = 50
 K: int = 3
 S: int = 1
-SolverModes: TypeAlias = Literal["original", "sci", "sciplus", "sciall"]
-K_RELU_SOLVER_MODES: List[SolverModes] = ["sciplus"]
-EPSILONS: List[float] = [0.00784313725]
+SOLVER_MODE: Literal["original", "sci", "sciplus", "sciall"] = "sciplus"
+EPSILON: float = 0.00784313725
 
 model_name = os.path.splitext(os.path.basename(MODEL_PATH))[0]
 
 def main() -> None:
-    for solver in K_RELU_SOLVER_MODES:
-        for img in IMG_IDS:
-            for eps in EPSILONS:
-                save_file_name = (
-                    model_name + f"_epsilon={eps}" + f"_imgid={img}" + "_domain=GRENA" + ".pkl"
-                )
+    for img in IMG_IDS:
+        command = f"""
+        source "{os.path.abspath(VIRTUAL_ENV_PATH)}";
+        cd {os.path.abspath("tf_verify")};
+        python Grena_runone_image.py
+            --domain refinepoly
+            --GRENA True
+            --dataset "{DATASET}"
+            --netname "{os.path.abspath(MODEL_PATH)}"
+            --output_dir "{os.path.abspath(SAVE_DIR)}"
+            --timeout_AR 600
 
-                command = f"""
-                source "{os.path.abspath(VIRTUAL_ENV_PATH)}";
-                cd {os.path.abspath("tf_verify")};
-                python Grena_runone_image.py
-                    --domain refinepoly
-                    --GRENA True
-                    --dataset "{DATASET}"
-                    --netname "{os.path.abspath(MODEL_PATH)}"
-                    --output_dir "{os.path.abspath(os.path.join(SAVE_DIR, solver))}"
-                    --bounds_save_filename "{save_file_name}"
-                    --timeout_AR 600
-
-                    {f'--use_wralu "{solver}"' if solver != "original" else ""}
-                    --epsilon "{eps}"
-                    --imgid "{img}"
-                    --sparse_n "{SPARSE_N}"
-                    --k "{K}"
-                    --s "{S}"
-                ;
-                """
-                command = re.sub(r"\n\s*", " ", command).strip()
-                print(f"Running:\n{command}")
-                subprocess.run(command, shell=True, executable="/bin/bash")
+            {f'--use_wralu "{SOLVER_MODE}"' if SOLVER_MODE != "original" else ""}
+            --epsilon "{EPSILON}"
+            --imgid "{img}"
+            --sparse_n "{SPARSE_N}"
+            --k "{K}"
+            --s "{S}"
+        ;
+        """
+        command = re.sub(r"\n\s*", " ", command).strip()
+        print(f"Running:\n{command}")
+        subprocess.run(command, shell=True, executable="/bin/bash")
 
 
 if __name__ == "__main__":
--- MConvSmall_bounds.py
+++ MConvSmall_bounds.py
@@ -1,8 +1,7 @@
 import os
 import re
 import subprocess
-from typing import List, Literal
-from typing_extensions import TypeAlias
+from typing import Literal
 
 VIRTUAL_ENV_PATH = "/home/yuyi/loris5/PRIMA/venv_3dot8/bin/activate"
 """Path to activate Python virtual env."""
@@ -17,40 +16,37 @@ IMG_ID: int = 76
 SPARSE_N: int = 50
 K: int = 3
 S: int = 1
-SolverModes: TypeAlias = Literal["original", "sci", "sciplus", "sciall"]
-K_RELU_SOLVER_MODES: List[SolverModes] = ["sciplus"]
-EPSILONS: List[float] = [0.11]
+SOLVER_MODE: Literal["original", "sci", "sciplus", "sciall"] = "sciplus"
+EPSILON: float = 0.11
 
 model_name = os.path.splitext(os.path.basename(MODEL_PATH))[0]
 
 def main() -> None:
-    for solver in K_RELU_SOLVER_MODES:
-        for eps in EPSILONS:
-            save_file_name = (
-                model_name + f"_epsilon={eps}" + f"_imgid={IMG_ID}" + "_domain=refinepoly" + ".pkl"
-            )
+    save_file_name = (
+        model_name + f"_epsilon={EPSILON}" + f"_imgid={IMG_ID}" + "_domain=refinepoly" + f"_solver={SOLVER_MODE}" + ".pkl"
+    )
 
-            command = f"""
-            source "{os.path.abspath(VIRTUAL_ENV_PATH)}";
-            cd {os.path.abspath("tf_verify")};
-            python Grena_runone_image.py
-                --domain refinepoly
-                --dataset "{DATASET}"
-                --netname "{os.path.abspath(MODEL_PATH)}"
-                --output_dir "{os.path.abspath(os.path.join(SAVE_DIR, solver))}"
-                --bounds_save_filename "{save_file_name}"
+    command = f"""
+    source "{os.path.abspath(VIRTUAL_ENV_PATH)}";
+    cd {os.path.abspath("tf_verify")};
+    python Grena_runone_image.py
+        --domain refinepoly
+        --dataset "{DATASET}"
+        --netname "{os.path.abspath(MODEL_PATH)}"
+        --output_dir "{os.path.abspath(SAVE_DIR)}"
+        --bounds_save_filename "{save_file_name}"
 
-                {f'--use_wralu "{solver}"' if solver != "original" else ""}
-                --epsilon "{eps}"
-                --imgid "{IMG_ID}"
-                --sparse_n "{SPARSE_N}"
-                --k "{K}"
-                --s "{S}"
-            ;
-            """
-            command = re.sub(r"\n\s*", " ", command).strip()
-            print(f"Running:\n{command}")
-            subprocess.run(command, shell=True, executable="/bin/bash")
+        {f'--use_wralu "{SOLVER_MODE}"' if SOLVER_MODE != "original" else ""}
+        --epsilon "{EPSILON}"
+        --imgid "{IMG_ID}"
+        --sparse_n "{SPARSE_N}"
+        --k "{K}"
+        --s "{S}"
+    ;
+    """
+    command = re.sub(r"\n\s*", " ", command).strip()
+    print(f"Running:\n{command}")
+    subprocess.run(command, shell=True, executable="/bin/bash")
 
 
 if __name__ == "__main__":
