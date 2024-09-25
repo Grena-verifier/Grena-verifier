7319ae88ab25b9b3af1faed5453d3e8b74b30614
Anonymous <anonymous@example.com>
2024-09-25 14:53:04 +0800

    Tweak download model script to merge 'resnet' dir w. 'cifar10'

--- download_models.sh
+++ download_models.sh
@@ -18,15 +18,15 @@ wget https://files.sri.inf.ethz.ch/eran/nets/onnx/mnist/convMedGRELU__Point.onnx
 wget https://files.sri.inf.ethz.ch/eran/nets/onnx/mnist/convBigRELU__DiffAI.onnx
 
 
-# Download CIFAR10 models.
+# Download CIFAR10 CNN models.
 mkdir -p "$script_dir/models/cifar10"
 cd "$script_dir/models/cifar10"
 wget https://files.sri.inf.ethz.ch/eran/nets/onnx/cifar/convMedGRELU__PGDK_w_0.0078.onnx
 wget https://files.sri.inf.ethz.ch/eran/nets/onnx/cifar/convBigRELU__DiffAI.onnx
 
 
-# Download and uncompress ResNet models.
-mkdir -p "$script_dir/models/resnet"
-cd "$script_dir/models/resnet"
+# Download and uncompress CIFAR10 ResNet models.
+mkdir -p "$script_dir/models/cifar10"
+cd "$script_dir/models/cifar10"
 wget -O - https://github.com/ChristopherBrix/vnncomp2022_benchmarks/raw/main/benchmarks/sri_resnet_a/onnx/resnet_3b2_bn_mixup_adv_4.0_bs128_lr-1.onnx.gz | gunzip > resnet_3b2_bn_mixup_adv_4.0_bs128_lr-1.onnx
 wget -O - https://github.com/ChristopherBrix/vnncomp2022_benchmarks/raw/main/benchmarks/sri_resnet_b/onnx/resnet_3b2_bn_mixup_ssadv_4.0_bs128_lr-1_v2.onnx.gz | gunzip > resnet_3b2_bn_mixup_ssadv_4.0_bs128_lr-1_v2.onnx
