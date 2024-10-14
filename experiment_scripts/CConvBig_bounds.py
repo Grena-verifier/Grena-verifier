3bf017a5ae5e42f1ee6bd00aeed0efa77662ba5c
Anonymous <anonymous@example.com>
2024-10-14 23:55:12 +0800

    Update CConvBig bounds script to try all imgs

--- experiment_scripts/CConvBig_bounds.py
+++ experiment_scripts/CConvBig_bounds.py
@@ -9,11 +9,13 @@ def relative_to_this_file(path: str) -> str:
 
 
 if __name__ == "__main__":
-    run_bounds_experiment(
-        model_path=relative_to_this_file("../models/cifar10/convBigRELU__DiffAI.onnx"),
-        dataset="cifar10",
-        use_normalised_dataset=True,
-        epsilon=0.00784313725,
-        img_id=80,
-        save_dir=relative_to_this_file("results/CConvBig/bounds"),
-    )
+    IMG_IDS = [0,12,13,17,20,32,33,45,47,49,55,65,71,80,88,91,97,101,102,103,104,115,119,130,132,145,155,163,165,174]  # fmt: skip
+    for img_id in IMG_IDS:
+        run_bounds_experiment(
+            model_path=relative_to_this_file("../models/cifar10/convBigRELU__DiffAI.onnx"),
+            dataset="cifar10",
+            use_normalised_dataset=True,
+            epsilon=0.00784313725,
+            img_id=img_id,
+            save_dir=relative_to_this_file(f"results/CConvBig/bounds/img_{img_id}"),
+        )
