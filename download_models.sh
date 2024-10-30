9c41805762b841a0796fd9e8757102df342883c7
Anonymous <anonymous@example.com>
2024-10-30 12:25:46 +0800

    Add code to download ResNet4B

--- download_models.sh
+++ download_models.sh
@@ -25,8 +25,9 @@ wget https://files.sri.inf.ethz.ch/eran/nets/onnx/cifar/convMedGRELU__PGDK_w_0.0
 wget https://files.sri.inf.ethz.ch/eran/nets/onnx/cifar/convBigRELU__DiffAI.onnx
 
 
-# Download and uncompress CIFAR10 ResNet models.
+# Download/Uncompress CIFAR10 ResNet models.
 mkdir -p "$script_dir/models/cifar10"
 cd "$script_dir/models/cifar10"
 wget -O - https://github.com/ChristopherBrix/vnncomp2022_benchmarks/raw/main/benchmarks/sri_resnet_a/onnx/resnet_3b2_bn_mixup_adv_4.0_bs128_lr-1.onnx.gz | gunzip > resnet_3b2_bn_mixup_adv_4.0_bs128_lr-1.onnx
 wget -O - https://github.com/ChristopherBrix/vnncomp2022_benchmarks/raw/main/benchmarks/sri_resnet_b/onnx/resnet_3b2_bn_mixup_ssadv_4.0_bs128_lr-1_v2.onnx.gz | gunzip > resnet_3b2_bn_mixup_ssadv_4.0_bs128_lr-1_v2.onnx
+wget -O resnet_4b.onnx https://raw.githubusercontent.com/stanleybak/vnncomp2021/main/benchmarks/cifar10_resnet/onnx/resnet_4b.onnx
