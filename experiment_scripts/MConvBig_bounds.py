7be44eda2665bf25ba72862dd78d78e1cd6a6d73
Anonymous <anonymous@example.com>
2024-11-24 16:35:41 +0800

    Create scripts for MConvBig

---
+++ experiment_scripts/MConvBig_bounds.py
@@ -0,0 +1,19 @@
+import os
+from helper import run_bounds_experiment
+
+
+def relative_to_this_file(path: str) -> str:
+    """Set path to be relative to this file."""
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.abspath(os.path.join(script_dir, path))
+
+
+if __name__ == "__main__":
+    run_bounds_experiment(
+        model_path=relative_to_this_file("../models/mnist/convBigRELU__DiffAI.onnx"),
+        dataset="mnist",
+        use_normalised_dataset=False,
+        epsilon=0.313,
+        img_id=174,
+        save_dir=relative_to_this_file("results/MConvBig/bounds"),
+    )
---
+++ experiment_scripts/MConvBig_verify.py
@@ -0,0 +1,19 @@
+import os
+from helper import run_verification_experiment
+
+
+def relative_to_this_file(path: str) -> str:
+    """Set path to be relative to this file."""
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.abspath(os.path.join(script_dir, path))
+
+
+if __name__ == "__main__":
+    run_verification_experiment(
+        model_path=relative_to_this_file("../models/mnist/convBigRELU__DiffAI.onnx"),
+        dataset="mnist",
+        use_normalised_dataset=False,
+        epsilon=0.313,
+        img_ids=[2,9,24,46,70,81,174,182,183,187,223,235,265,295,296,310,311,326,340,363,386,439,458,466,481,522,524,545,587,591],
+        save_dir=relative_to_this_file("results/MConvBig/verify"),
+    )  # fmt: skip
